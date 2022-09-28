f3ad79439cfe55995ac79acb37a8b08d5278ac6b
Not Null <nolbertomesino29@gmail.com>
2022-09-28 17:25:46 -0500

    ajustment commit

--- starter-code/assets/js/destinationChange.js
+++ starter-code/assets/js/destinationChange.js
@@ -1,161 +1,220 @@
 console.log('Conected')
 
-// Contenido del Html -> preparado para enviar el fragment
-// const contentArticle = document.querySelector('.article-section__body');
 
- // 1 step
-const dataJson = fetch('./data.json');
-// const data = dataJson.clone();
-
-//change img
-const contentImg = document.getElementById('img');
-const figcaptionImg = document.getElementById('content_img');
-const imgNew = document.getElementById('img')
-
-// change contents
-const parragraph = document.querySelector('.parragraph')
-const section__footer = document.querySelector('.section__footer')
-const title = document.querySelector('.title')
-
-// CALL A THE LINKS
-const btnLinkMoon = document.getElementById("moon")
-const btnLinkMars = document.getElementById("mars")
-const btnLinkEuropa = document.getElementById("europa")
-const btnLinkTitan = document.getElementById("titan")
+// 1 step
+try{
+    // Contenido del Html -> preparado para enviar el fragment
+    const contentArticle = document.querySelector('.article-section');
+    const contentArticleBody = document.querySelector('.article-section__body');
 
 
-btnLinkMoon.addEventListener('click', () =>{
-    // console.log("click")
-    dataJson
-        .then(res =>res.clone().json())
-        .then(res => getData(res.destinations))
+    const Createfragment  = new DocumentFragment()
+    // const Createfragment = document.createDocumentFragment()
 
-    // console.log(dataJson)
-
-    const getData = (data) => {
-            
-        let viweData = data[0]
-        console.log(viweData)      
+    const dataJson = fetch('./data.json');
     
-        imgNew.src = viweData.images.png
-        parragraph.innerHTML = viweData.description
-        title.innerHTML = viweData.name
-        section__footer.innerHTML = `
-            <div class="footer__left">
-                <h4>Avg. distance</h4>
-                <p>384,400 km</p>
-            </div>
 
-            <div class="footer__rigth">
-                <h4>Est. travel time</h4>
-                <p>3 days</p>
-                
-            </div>
-        `
+    //change img
+    const contentImg = document.getElementById('img');
+    const figcaptionImg = document.getElementById('content_img');
+    const imgNew = document.getElementById('img')
 
-        figcaptionImg.replaceChild(img, contentImg)
+    // change contents
+    const parragraph = document.querySelector('.parragraph')
+    const section__footer = document.querySelector('.section__footer')
+    const title = document.querySelector('.title')
 
-    }
+    // CALL A THE LINKS
+    const btnLinkMoon = document.getElementById("moon")
+    const btnLinkMars = document.getElementById("mars")
+    const btnLinkEuropa = document.getElementById("europa")
+    const btnLinkTitan = document.getElementById("titan")
 
-})
 
-btnLinkMars.addEventListener('click', () =>{
-    dataJson
-        .then(res =>res.clone().json())
-        .then(res => getData(res.destinations))
+    btnLinkMoon.addEventListener('click', () =>{
+        // console.log("click")
+        dataJson
+            .then(res =>res.clone().json())
+            .then(res =>
+                        Createfragment.append(
+                            getData(res.destinations)
+                            
+                            
+                            ),
+                        contentArticle.replaceChildren(contentArticleBody)
+                )
+        
+        // console.log(dataJson)
 
-    // console.log(dataJson)
+        const getData = (data) => {
+            try {
+                let viweData = data[0]   
+        
+                imgNew.src = viweData.images.png
+                parragraph.innerHTML = viweData.description
+                title.innerHTML = viweData.name
+                section__footer.innerHTML = `
+                    <div class="footer__left">
+                        <h4>Avg. distance</h4>
+                        <p>384,400 km</p>
+                    </div>
 
-    const getData = (data) => {
-            
-        let viweData = data[1] 
+                    <div class="footer__rigth">
+                        <h4>Est. travel time</h4>
+                        <p>3 days</p>
+                        <p>https://mohammedduke.github.io/Space-tourism-multi-page-website/destination-moon.html</p>
+                        
+                        
+                    </div>
+                `
 
-        imgNew.src = viweData.images.png
-        parragraph.innerHTML = viweData.description
-        title.innerHTML = viweData.name
-        section__footer.innerHTML = `
-                <div class="footer__left">
-                    <h4>Avg. distance</h4>
-                    <p>225 mil. km</p>
-                </div>
+                figcaptionImg.replaceChild(img, contentImg)
+            } catch (e) {
+                console.log(e)
+            } 
 
-                <div class="footer__rigth">
-                    <h4>Est. travel time</h4>
-                    <p>9 months</p>
+        }
+
+
+
+    })
+
+    btnLinkMars.addEventListener('click', () =>{
+        dataJson
+            .then(res =>res.clone().json())
+            .then(res =>
+                Createfragment.append(
+                    getData(res.destinations)
                     
-                </div>
-            `
+                    
+                    ),
+                contentArticle.replaceChildren(contentArticleBody)
+                )
 
-        figcaptionImg.replaceChild(img, contentImg)
+        // console.log(dataJson)
 
-    }
-
-})
-
-
-btnLinkEuropa.addEventListener('click', () =>{
-     dataJson
-        .then(res =>res.clone().json())
-        .then(res => getData(res.destinations))
-
-    // console.log(dataJson)
-
-    const getData = (data) => {
-            
-        let viweData = data[2]     
+        const getData = (data) => {
+            try{
+                let viweData = data[1]   
     
-        imgNew.src = viweData.images.png
-        parragraph.innerHTML = viweData.description
-        title.innerHTML = viweData.name
-        section__footer.innerHTML = `
-                <div class="footer__left">
-                    <h4>Avg. distance</h4>
-                    <p>628 mil. km</p>
-                </div>
-
-                <div class="footer__rigth">
-                    <h4>Est. travel time</h4>
-                    <p>3 years</p>
-                    
-                </div>
-            `
-
-        figcaptionImg.replaceChild(img, contentImg)
-
-    }
-})
-
-btnLinkTitan.addEventListener('click', () =>{
-
-     dataJson
-        .then(res =>res.clone().json())
-        .then(res => getData(res.destinations))
-
-    const getData = (data) => {
-            
-        let viweData = data[3];
+                imgNew.src = viweData.images.png
+                parragraph.innerHTML = viweData.description
+                title.innerHTML = viweData.name
+                section__footer.innerHTML = `
+                        <div class="footer__left">
+                            <h4>Avg. distance</h4>
+                            <p>225 mil. km</p>
+                        </div>
     
-        imgNew.src = viweData.images.png;
-        parragraph.innerHTML = viweData.description;
-        title.innerHTML = viweData.name;
+                        <div class="footer__rigth">
+                            <h4>Est. travel time</h4>
+                            <p>9 months</p>
+                            
+                        </div>
+                    `
+    
+                figcaptionImg.replaceChild(img, contentImg)
+            }catch(e){
+                console.log(e)
+            }
 
-        section__footer.innerHTML = `
-                <div class="footer__left">
-                    <h4>Avg. distance</h4>
-                    <p>1.6 bil. km</p>
-                </div>
+        }
 
-                <div class="footer__rigth">
-                    <h4>Est. travel time</h4>
-                    <p>7 years</p>
+    })
+
+
+    btnLinkEuropa.addEventListener('click', () =>{
+        dataJson
+            .then(res =>res.clone().json())
+            .then(res =>
+                Createfragment.append(
+                    getData(res.destinations)
                     
-                </div>
-            `;
-
-        figcaptionImg.replaceChild(img, contentImg)
-
-    }
-
-})
+                    
+                    ),
+                contentArticle.replaceChildren(contentArticleBody)
+                )
 
 
+        // console.log(dataJson)
+
+        const getData = (data) => {
+            try {
+                let viweData = data[2]     
+        
+                imgNew.src = viweData.images.png
+                parragraph.innerHTML = viweData.description
+                title.innerHTML = viweData.name
+                section__footer.innerHTML = `
+                        <div class="footer__left">
+                            <h4>Avg. distance</h4>
+                            <p>628 mil. km</p>
+                        </div>
+
+                        <div class="footer__rigth">
+                            <h4>Est. travel time</h4>
+                            <p>3 years</p>
+                            
+                        </div>
+                    `
+
+                figcaptionImg.replaceChild(img, contentImg)
+
+            } catch (e) {
+                console.log(e)
+            }  
+            
+        }
+
+    })
+
+    btnLinkTitan.addEventListener('click', () =>{
+        dataJson
+            .then(res =>res.clone().json())
+            .then(res =>
+                Createfragment.append(
+                    getData(res.destinations)
+                    
+                    
+                    ),
+                contentArticle.replaceChildren(contentArticleBody)
+                )
+
+
+        const getData = (data) => {
+            try{
+                let viweData = data[3];
+        
+                imgNew.src = viweData.images.png;
+                parragraph.innerHTML = viweData.description;
+                title.innerHTML = viweData.name;
+
+                section__footer.innerHTML = `
+                        <div class="footer__left">
+                            <h4>Avg. distance</h4>
+                            <p>1.6 bil. km</p>
+                        </div>
+
+                        <div class="footer__rigth">
+                            <h4>Est. travel time</h4>
+                            <p>7 years</p>
+                            
+                        </div>
+                    `;
+
+                figcaptionImg.replaceChild(img, contentImg)
+        
+            } catch (e) {
+                console.log(e)
+            }
+
+        }
+
+    })
+
+}catch(e){
+    console.log(e)
+
+}finally{
+    console.log("DOWNLOAD EXIT")
+}
